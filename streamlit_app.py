756380c35cbe5c9f730d8bdc5c8eabb1bdf9bc53
david33266 <dspvak@gmail.com>
2026-01-04 00:45:54 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+import streamlit as st
+st.write("✅ streamlit_app.py נטען")
